fbbb99bbbcead6b23e53163b772b767b731b4183
Maya Perry <MayaRP@gmail.com>
2026-03-05 16:40:54 -0500

    implemented publishQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -111,7 +111,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQuestion: Question = {
+        ...question,
+        published: !question.published,
+    };
+    return newQuestion;
 }
 
 /**

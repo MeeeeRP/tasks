48e83ee8b91f025e82061a2eaecf83ff0ea9a2c3
Maya Perry <MayaRP@gmail.com>
2026-03-15 13:41:37 -0400

    Edit Mode Implement

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { CheckAnswer } from "./form-components/CheckAnswer";
 import { GiveAttempts } from "./form-components/GiveAttempts";
-// import { EditMode } from "./form-components/EditMode";
+import { EditMode } from "./form-components/EditMode";
 // import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 // import { ChangeColor } from "./form-components/ChangeColor";
 function App(): React.JSX.Element {
@@ -33,7 +33,7 @@ function App(): React.JSX.Element {
             <hr></hr>
             <GiveAttempts></GiveAttempts>
             <hr></hr>
-            {/* <EditMode></EditMode> */}
+            <EditMode></EditMode>
             <hr></hr>
             {/* <ChangeColor></ChangeColor> */}
             <hr></hr>
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,61 @@
 import React, { useState } from "react";
+import { Form, Col, Row } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [name, setName] = useState<string>("Your Name");
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Group controlId="editMode">
+                <Form.Label>Edit Mode</Form.Label>
+                <Form.Check
+                    type="switch"
+                    label="Edit Mode"
+                    checked={isEditing}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setIsEditing(event.target.checked);
+                    }}
+                />
+            </Form.Group>
+            {isEditing && (
+                <Row>
+                    <Col>
+                        <Form.Group controlId="nameInput">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={name}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>,
+                                ) => {
+                                    setName(event.target.value);
+                                }}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="studentStatus">
+                            <Form.Label>Student Status</Form.Label>
+                            <Form.Check
+                                type="checkbox"
+                                label="Is a student"
+                                checked={isStudent}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>,
+                                ) => {
+                                    setIsStudent(event.target.checked);
+                                }}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+            )}
+            {!isEditing && (
+                <p>
+                    {name} is {isStudent ? "" : "not"} a student.
+                </p>
+            )}
         </div>
     );
 }
